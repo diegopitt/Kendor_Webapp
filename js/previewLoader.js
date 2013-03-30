12718a261fa8f75477f5aaf38b30dcadbfd53792
Eddie <eddie_D_2000@hotmail.com>
2013-03-30 10:43:51 -0500

    Busting out the ImageLoading to a separate file

---
+++ js/previewLoader.js
@@ -0,0 +1,10 @@
+var galaxy = new CanvasZoom( {
+        'canvas' : document.getElementById('zoomCanvas'),
+        'tilesFolder' : "../Doors/Alder_FM_J_W_RAISEDPANELDRF_24",
+        'imageWidth' : 383,
+        'imageHeight' : 236.5,
+        'drawBorder' : false,
+        /*'defaultZoom' : 8,
+        'maxZoom' : 8*/
+        'minZoom' : 5
+} );
