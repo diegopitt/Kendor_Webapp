8c782f2d6965e5309647803def614d3d1e9e0e3e
Eddie <eddie_D_2000@hotmail.com>
2013-06-02 23:15:14 -0500

    Adjusting the previewLoader settings for the 2 doors I have in

--- Doors/Alder_FM_J_W_RAISEDPANELDRF_24/previewLoader.js
+++ Doors/Alder_FM_J_W_RAISEDPANELDRF_24/previewLoader.js
@@ -1,8 +1,8 @@
 var galaxy = new CanvasZoom( {
         'canvas' : document.getElementById('zoomCanvas'),
         'tilesFolder' : "/Doors/Alder_FM_J_W_RAISEDPANELDRF_24",
-        'imageWidth' : 383,
-        'imageHeight' : 236.5,
+        'imageWidth' : 3830,
+        'imageHeight' : 2360,
         'drawBorder' : false,
         /*'defaultZoom' : 8,
         'maxZoom' : 8*/
--- Doors/Alder_FS_I_U_SQUARE_24/previewLoader.js
+++ Doors/Alder_FS_I_U_SQUARE_24/previewLoader.js
@@ -1,8 +1,8 @@
 var galaxy = new CanvasZoom( {
         'canvas' : document.getElementById('zoomCanvas'),
         'tilesFolder' : "/Doors/Alder_FS_I_U_SQUARE_24",
-        'imageWidth' : 236.5,
-        'imageHeight' : 383,
+        'imageWidth' : 1800,
+        'imageHeight' : 2400,
         'drawBorder' : false,
         /*'defaultZoom' : 8,
         'maxZoom' : 8*/
