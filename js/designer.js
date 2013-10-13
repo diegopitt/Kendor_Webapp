6ea88db7b61e61f36493d5f3b833194f3d5f04a0
Eddie <eddie_D_2000@hotmail.com>
2013-10-12 22:35:02 -0500

    Changing titles on click

--- js/designer.js
+++ js/designer.js
@@ -1,17 +1,23 @@
 
 
-$('#styleList').on('click', 'li', function() {
+$('#styleList').on('click', 'li', function(evt) {
     $('#styleList').trigger('collapse'); // id of clicked li by directly accessing DOMElement property
+    $('#styleText .ui-btn-text').text("Select Style - " + $(evt.currentTarget).text());
 });
-$('#panelList').on('click', 'li', function() {
+$('#panelList').on('click', 'li', function(evt) {
     $('#panelList').trigger('collapse'); // id of clicked li by directly accessing DOMElement property
+    $('#panelText .ui-btn-text').text("Select Panel - " + $(evt.currentTarget).text());
 });
-$('#innerList').on('click', 'li', function() {
+$('#innerList').on('click', 'li', function(evt) {
     $('#innerList').trigger('collapse'); // id of clicked li by directly accessing DOMElement property
+    $('#innerText .ui-btn-text').text("Select Inner - " + $(evt.currentTarget).text());
 });
-$('#outerList').on('click', 'li', function() {
+$('#outerList').on('click', 'li', function(evt) {
     $('#outerList').trigger('collapse'); // id of clicked li by directly accessing DOMElement property
+    $('#outerText .ui-btn-text').text("Select Outer - " + $(evt.currentTarget).text());
 });
-$('#speciesList').on('click', 'li', function() {
+$('#speciesList').on('click', 'li', function(evt) {
     $('#speciesList').trigger('collapse'); // id of clicked li by directly accessing DOMElement property
+
+    $('#speciesText .ui-btn-text').text("Select Species - " + $(evt.currentTarget).text());
 });
