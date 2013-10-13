5a93a43697627c2119e8d36d5530401731d1fd41
Eddie <eddie_D_2000@hotmail.com>
2013-10-12 20:34:29 -0500

    Getting all the door design parts in and adding some logic in
    Adding the door designer image parts and the logic to collapse the
    designer items when you click on one.

---
+++ js/designer.js
@@ -0,0 +1,17 @@
+
+
+$('#styleList').on('click', 'li', function() {
+    $('#styleList').trigger('collapse'); // id of clicked li by directly accessing DOMElement property
+});
+$('#panelList').on('click', 'li', function() {
+    $('#panelList').trigger('collapse'); // id of clicked li by directly accessing DOMElement property
+});
+$('#innerList').on('click', 'li', function() {
+    $('#innerList').trigger('collapse'); // id of clicked li by directly accessing DOMElement property
+});
+$('#outerList').on('click', 'li', function() {
+    $('#outerList').trigger('collapse'); // id of clicked li by directly accessing DOMElement property
+});
+$('#speciesList').on('click', 'li', function() {
+    $('#speciesList').trigger('collapse'); // id of clicked li by directly accessing DOMElement property
+});
